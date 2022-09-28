919370e0f739dc4246d515ea5c9dc5cc39e10f67
Alex Stevens <alexstevens@smaz.us>
2022-09-27 22:58:28 -0500

    project refactored with react

--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
 import Phaser from 'phaser';
+import React from 'react';
 import logoImg from './assets/logo.png';
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const config = {
-    type: Phaser.AUTO,
-    parent: "phaser-example",
-    width: 800,
-    height: 600,
-    scene: {
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
       preload: preload,
-      create: create
-    }
-  };
-  const game = new Phaser.Game(config);
-  function preload() {
-    this.load.image("logo", logoImg);
+      create: create,
+      update: update
   }
-  function create() {
-    const logo = this.add.image(400, 150, "logo");
+};
+
+var game = new Phaser.Game(config);
+
+function preload (){
+  this.load.image("logo", logoImg);
+}
+
+function create (){
+  const logo = this.add.image(400, 150, "logo");
   this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1
-    });
-  }
-  ReactDOM.render(<App />, document.getElementById("root"));
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1
+  });
+}
+
+function update ()
+{
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+
