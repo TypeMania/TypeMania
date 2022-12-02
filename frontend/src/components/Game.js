426e4cef24e8bfe1b8adbcc26ffa9b43d7974858
Nargis Sultani <nargissultani@Nargiss-MBP.attlocal.net>
2022-12-01 23:06:49 -0600

    completed adding characters to notes

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -2,11 +2,7 @@
 import Phaser from 'phaser';
 import React, { Component } from 'react';
 import { scroll_values } from './SpeedSlider';
-import SongValues from './SongSelect';
-
-
-
-
+import { randomizedCharacters, song_values } from './SongSelect';
 //phaser game component
 
 //react component (different syntax than other compoenents, but basically the same)
@@ -90,22 +86,22 @@ export default class Game extends Component {
       hitzone_animations();
 
       noteArray = [];
-      const characters = randomizedCharacters();
-      console.log("characters length: " + characters.length)
+      //var chars = song_values.current_song_char;
+      //console.log("characters length: " + chars.length)
       var counter = 0;
       setInterval(()=>{
         //const note = this.add.rectangle(WIDTH+WIDTH/8,HEIGHT/3,60,60, 0xFFFF00).setStrokeStyle(3, 0x000000);
         
-        
+        console.log("characters length: " + song_values.current_song_char.length)
         this.rectangle = this.add.rectangle(0,0,60,60, 0xFFFF00).setStrokeStyle(3, 0x000000);
         var textConfig = {fontSize:'20px', color:'black', fontFamily: 'Arial'};
-        if (counter < characters.length){
-          this.Text = this.add.text(0, 0, characters[counter], textConfig);
+        if (counter < song_values.current_song_char.length){
+          this.Text = this.add.text(0, 0, song_values.current_song_char[counter], textConfig);
           counter = counter + 1;
         }
         else {
           counter = 0;
-          this.Text = this.add.text(0, 0, characters[counter], textConfig);
+          this.Text = this.add.text(0, 0, song_values.current_song_char[counter], textConfig);
           counter = counter + 1;
         }
         
@@ -121,6 +117,11 @@ export default class Game extends Component {
         
       }, scroll_values.note_scroll*1000);
 
+    }
+
+    song_values.updateSong = (songmap) => {
+      song_values.current_song_char = randomizedCharacters(songmap.bpm, songmap.length);
+      console.log("updated chars length: " + song_values.current_song_char.length);
     }
     
     scroll_values.applySpeed = (multiplier) => {
@@ -144,139 +145,6 @@ export default class Game extends Component {
 
       
     }
-
-    
-
-    //precondition: recieves a Songmap object. Songmap class must have a static constant property: INITIAL_ARRAY
-    //postcondition: returns a shuffled array of integers [0-60)
-    /*function seededPRNG({INITIAL_ARRAY, bpm, length, seed}){
-      const totalPossibleKeyAmount = 128 // amount of playable characters on keyboard (subject to change)
-      const arraySize = Math.round(bpm * length / 60); // the array size is calculated from the speed and length of the song
-      console.log("arraySize: " + arraySize)
-      const start = seed % totalPossibleKeyAmount
-      console.log("start: " + start)
-      const stop = start + arraySize;
-      console.log("stop: " + stop)
-      //const unshuffledArray = Songmap.INITIAL_ARRAY.slice(start,stop); // the larger static array is cut down to the previously calculated size
-      const unshuffledArray = INITIAL_ARRAY.slice(start,stop); // the larger static array is cut down to the previously calculated size
-      
-      //helper function: recursive shuffling algorithm without randomness.
-      //precondition: recieves an array and integer
-      //postcondition: returns the shuffled array
-      function shuffle(arr, shuffleCount){
-          if(shuffleCount <= 0){ // recursion exit condition
-              return arr;
-          }
-          else {
-              const normalizedSeed = shuffleCount % totalPossibleKeyAmount;
-              const half = Math.floor(normalizedSeed/2);
-              arr = arr.reverse(); //pointless reverse that may even lessen the impact the below swaps
-              //not really well thought out swapping loop of slightly offset left traversing and right traversing indices
-              for (let [i,j] = [normalizedSeed,arr.length-normalizedSeed]; i<arr.length/2 & j>=arr.length/2; i+=(normalizedSeed-half),j-=(normalizedSeed+half)){
-                  [arr[i], arr[j]] = [arr[j], arr[i]];
-              }
-              return shuffle(arr,shuffleCount-1); //recursion continuation statement
-          }
-      }
-
-      console.log("unshuffledArray length: " + unshuffledArray.length)
-      
-      return shuffle(unshuffledArray, seed); //the cut down array is uniformly shuffled x amount of times where x is the songmap's seed
-    }*/
-
-    function seededPRNG({bpm, length, seed}){
-      const totalPossibleKeyAmount = 128 // amount of playable characters on keyboard (subject to change)
-      const arraySize = Math.round(bpm * length / 60); // the array size is calculated from the speed and length of the song
-      console.log("arraySize: " + arraySize)
-      var seedArr = [];
-      
-      for (var i = 0; i < arraySize; i++) {
-        seedArr.push(Math.random() * (127 - 0) + 0);
-        //Do something
-      }
-      
-      return seedArr;
-      
-    }
-
-
-    //precondition: recieves an array of integer returned by seededPRNG function, and the value from songmap settings
-    //postcondition: returns an array of randomized characters to go into graphic
-    /*function randomizedCharacters(songmapSettings, seedArr){
-      const charArray = [];
-      for (let i = 0; i < seedArr.length; i++) {
-        if (songmapSettings === "UPPER"){
-          if (seedArr[i] >= 65 && seedArr[i] <= 90) {
-            const char = String.fromCharCode(seedArr[i]);
-            charArray.push(char);
-          }
-          else {
-            const randomChar = String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1)) + 65);
-            charArray.push(randomChar);
-          }
-      }
-      else if (songmapSettings === "LOWER"){
-        if (seedArr[i] >= 97 && seedArr[i] <= 122) {
-          const char = String.fromCharCode(seedArr[i]);
-          charArray.push(char);
-        }
-        else {
-          const randomChar = String.fromCharCode(Math.floor(Math.random() * (122 - 97 + 1)) + 97);
-          charArray.push(randomChar);
-        }
-      }
-      else if (songmapSettings === "UPPER_LOWER_NUMERIC"){
-        
-        if ((seedArr[i] >= 65 && seedArr[i] <= 90) 
-            || (seedArr[i] >= 97 && seedArr[i] <= 122) 
-            || (seedArr[i] >= 49 && seedArr[i] <= 57)) {
-              const char = String.fromCharCode(seedArr[i]);
-              charArray.push(char);
-        }
-        else {
-          const randomAsciiCode = [];
-          const upperChar = Math.floor(Math.random() * (90 - 65 + 1)) + 65;
-          randomAsciiCode.push(upperChar);
-          const lowerChar = Math.floor(Math.random() * (122 - 97 + 1)) + 97;
-          randomAsciiCode.push(lowerChar);
-          const numChar = Math.floor(Math.random() * (57 - 49 + 1)) + 49;
-          randomAsciiCode.push(numChar);
-          const randomChar = String.fromCharCode(randomAsciiCode[Math.floor(Math.random() * randomAsciiCode.length)]);
-          charArray.push(randomChar);
-        }
-      }
-      else if (songmapSettings === "ALL_CHARS"){
-        const char = String.fromCharCode(seedArr[i]);
-        charArray.push(char);
-      }
-    }
-    return charArray;
-  }*/
-  
-  var INITIAL_ARRAY = [];
-  function randomizedCharacters(){
-    const currentSong = SongValues.currentSong;
-
-    
-    console.log("currentSong: " + currentSong);
-    //let bpm = currentSong.bpm;
-    //let length = currentSong.length;
-    //let seed = currentSong.seed;
-    let bpm = 145;
-    let length = 139;
-    let seed = 1;
-    const seedArr = seededPRNG({INITIAL_ARRAY, bpm, length, seed})
-    console.log("seedArr length: " + seedArr.length)
-    const charArray = [];
-    for (let i = 0; i < seedArr.length; i++) {
-      const char = String.fromCharCode(seedArr[i]);
-      charArray.push(char);
-    }
-    return charArray;
-  }
- 
-
-    
   }
 
   shouldComponentUpdate() {
--- frontend/src/components/SongSelect.js
+++ frontend/src/components/SongSelect.js
@@ -7,17 +7,45 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import axios from "axios";
 
-export const SongValues = () => {
-    const {currentSongmap} = useMusicPlayer();
-    return {
-        currentSong: currentSongmap
-    }
 
+function seededPRNG(bpm, length){
+    const arraySize = Math.round(bpm * length / 60); // the array size is calculated from the speed and length of the song
+    console.log("arraySize: " + arraySize)
+    var seedArr = [];
+    
+    for (var i = 0; i < arraySize; i++) {
+      seedArr.push(Math.random() * (127 - 0) + 0);
+    }
+    
+    return seedArr;
+    
 }
+
+export function randomizedCharacters(bpm, length){
+    
+    
+    const seedArr = seededPRNG(bpm, length)
+    const charArray = [];
+    for (let i = 0; i < seedArr.length; i++) {
+      const char = String.fromCharCode(seedArr[i]);
+      charArray.push(char);
+    }
+    return charArray;
+  }
+
+export const song_values = {
+    current_song_char: randomizedCharacters(145, 139),
+    updateSong: (songmap) => {
+        song_values.current_song_char = randomizedCharacters(songmap.bpm, songmap.length);
+        console.log("current song char length: " + song_values.current_song_char.length);
+    }
+}
+
 
 const SongsList = () => {
     const {playMusic} = useMusicPlayer();
     const [songs, setSong] = useState([]);
+    //const [currentSongmap, setCurrentSongmap] = useState([]);
     useEffect(() => {
         getSongs();
       }, []);
@@ -29,65 +57,69 @@ const SongsList = () => {
 
     //states 
     
-
     const [expanded, setExpanded] = useState(false);
 
     const handleChange =
         (panel) => (event , newExpanded) => {
+        console.log("newExpanded: " + newExpanded)
         setExpanded(newExpanded ? panel : false);
         };
 
     function handlePlayMusic(songmap){
-
-        //setCurrentSong(songmap);
+        //setCurrentSongmap(songmap);
+        song_values.updateSong(songmap);
         playMusic(songmap);
+
     }
 
 
      
 return (
-    <div className="song-select">
-        <Accordion defaultExpanded={false}>
-            <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1bh-content"
-            id="panel1bh-header"
-            >
-            <Typography>Song Select</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
+    <>
+        <div className="song-select">
+            <Accordion defaultExpanded={false}>
+                <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1bh-content"
+                id="panel1bh-header"
+                >
+                <Typography>Song Select</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
 
-                {songs?.map((songmap, index) => (
-                    <Accordion expanded={expanded === songmap.title} onChange={handleChange(songmap.title)}>
-                    <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1bh-content"
-                    id="panel1bh-header"
-                    >
-                    <button className="button" onClick={() => handlePlayMusic(songmap)}>
-                        {songmap.title}
-                    </button>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Box>
+                    {songs?.map((songmap, index) => (
+                        <Accordion expanded={expanded === songmap.title} onChange={handleChange(songmap.title)}>
+                        <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1bh-content"
+                        id="panel1bh-header"
+                        >
+                        <button className="button" onClick={() => handlePlayMusic(songmap)}>
+                            {songmap.title}
+                        </button>
+                        </AccordionSummary>
+                        <AccordionDetails>
                             <Box>
-                                <Typography>Artist: {songmap.artist}</Typography>
+                                <Box>
+                                    <Typography>Artist: {songmap.artist}</Typography>
+                                </Box>
+                                <Box>
+                                    <Typography>bpm: {songmap.bpm}</Typography>
+                                </Box>
+                                <Box>
+                                    <Typography>length: {songmap.length}</Typography>
+                                </Box>
+                        
                             </Box>
-                            <Box>
-                                <Typography>bpm: {songmap.bpm}</Typography>
-                            </Box>
-                            <Box>
-                                <Typography>length: {songmap.length}</Typography>
-                            </Box>
-                    
-                        </Box>
-                    </AccordionDetails>
-                    </Accordion>
-                ))}
+                        </AccordionDetails>
+                        </Accordion>
+                    ))}
 
-            </AccordionDetails>
-        </Accordion>     
-    </div>
+                </AccordionDetails>
+            </Accordion>     
+        </div>
+    </>
+    
     );  
        
 }
